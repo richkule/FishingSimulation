b7f5431cf048e1bac1f9f47287867e347a7cc873
Aleksandr Korneev <www.mailss.ru@mail.ru>
2021-11-21 17:21:02 +0300

    Add form

---
+++ FishingSimulation/MainForm.cpp
@@ -0,0 +1,20 @@
+#include "MainForm.h"
+
+using namespace System;
+
+using namespace System::Windows::Forms;
+
+
+
+[STAThreadAttribute]
+
+void Main(array< String ^> ^ args) {
+
+	Application::EnableVisualStyles();
+
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	FishingSimulation::MainForm form;
+	Application::Run(%form);
+
+}
